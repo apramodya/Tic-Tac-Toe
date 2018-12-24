e3ddfe00f611f8850e5c4dd818e4515c3895d866
apramodya <pramodyaabeysinghe@gmail.com>
2018-12-24 18:46:15 +0530

    find the winner of game

--- Tic Tac Toc/ViewController.swift
+++ Tic Tac Toc/ViewController.swift
@@ -9,14 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    var isComputerPlayer: Bool = false
-    var computer = [Int]()
-    var player = [Int]()
+    
+    var isPhonePlayer: Bool = false
+    var phonesCells = [Int]()
+    var playersCells = [Int]()
     var buttonsArray: [UIButton] = []
     var winner: String = ""
     var numberOfClicks: Int = 0
-    var availableNumbers = [Int]()
+    var availableCells: [Int] = [1,2,3,4,5,6,7,8,9]
     
     @IBOutlet weak var b1: UIButton!
     @IBOutlet weak var b2: UIButton!
@@ -36,72 +36,75 @@ class ViewController: UIViewController {
     }
     
     @IBAction func pressButton(_ sender: Any) {
+        numberOfClicks += 1
+        
         // player's chance
         let selectedButton = sender as! UIButton
         playGame(button: selectedButton)
-        numberOfClicks += 1
         
-        winner = winnerOfTheGame()
-        
-        if winner == "player" {
-            endGame(by: winner)
+        // check whether player is a winner
+        if numberOfClicks >= 3 && winnerOfTheGame() == "player" {
+            endGame(message: "You won the game")
         }
         else {
             // phone's chance
-            let computerSelectedButton = buttonsArray[selectRandomNumber() - 1]
-            playGame(button: computerSelectedButton)
+            let randomNumber = selectRandomNumber()!
+            let computersButton = buttonsArray[randomNumber - 1]
+            playGame(button: computersButton)
             
-            winner = winnerOfTheGame()
-            
-            if winner == "computer" {
-                endGame(by: winner)
+            // check whether phone is a winner
+            if winnerOfTheGame() == "phone" {
+                endGame(message: "Phone won the game")
             }
         }
         
-        if numberOfClicks == 3 {
-            endGame(by: "draw")
+        if winnerOfTheGame() == "" && numberOfClicks == 4 {
+            for i in availableCells {
+                buttonsArray[i-1].isEnabled = false
+            }
+            endGame(message: "Game is drawn")
         }
-        
         
     }
     
     func playGame(button selectedButton: UIButton) {
 
-        if isComputerPlayer {
+        if isPhonePlayer {
             selectedButton.setTitle("O", for: UIControl.State.normal)
             selectedButton.backgroundColor = UIColor(red: 102/255, green: 250/255, blue: 51/255, alpha: 0.5)
-            computer.append(selectedButton.tag)
-            isComputerPlayer = !isComputerPlayer
+            phonesCells.append(selectedButton.tag)
+            availableCells = availableCells.filter{ $0 != selectedButton.tag }
+           
+            isPhonePlayer = !isPhonePlayer
         } else {
             selectedButton.setTitle("X", for: UIControl.State.normal)
             selectedButton.backgroundColor = UIColor(red: 32/255, green: 192/255, blue: 243/255, alpha: 0.5)
-            player.append(selectedButton.tag)
-            isComputerPlayer = !isComputerPlayer
+            playersCells.append(selectedButton.tag)
+            availableCells = availableCells.filter{ $0 != selectedButton.tag }
+           
+            isPhonePlayer = !isPhonePlayer
         }
         
         selectedButton.isEnabled = false
     }
     
-    func selectRandomNumber() -> Int {
-        
-        for index in 1...9 {
-            if computer.contains(index) || player.contains(index) {
-                continue
-            }
-            availableNumbers.append(index)
-        }
-        let randomNumber = availableNumbers.randomElement()
-        
-        return randomNumber ?? 0
+    func selectRandomNumber() -> Int? {
+        return availableCells.randomElement()
     }
     
     func winnerOfTheGame() -> String {
         for possibility in winningPossibilities {
-            if computer.sorted() == possibility {
-                winner = "computer"
-                break
-            } else if player.sorted() == possibility {
+            // check whether player is the winner
+            let isPlayerWinner = containsNumbers(cellsArray: playersCells.sorted(), possibilityArray: possibility)
+            if isPlayerWinner {
                 winner = "player"
+                break
+            }
+            
+            // check whether phone is the winner
+            let isPhoneWinner = containsNumbers(cellsArray: phonesCells.sorted(), possibilityArray: possibility)
+            if isPhoneWinner {
+                winner = "phone"
                 break
             }
         }
@@ -109,47 +112,39 @@ class ViewController: UIViewController {
         return winner
     }
     
-    func endGame(by result: String) {
-        displayWinner(result)
+    func containsNumbers(cellsArray: [Int], possibilityArray: [Int]) -> Bool {
+        var isHaving = false
+        
+        for i in possibilityArray {
+            isHaving = cellsArray.contains(i)
+            
+            if !isHaving {
+                break
+            }
+        }
+        
+        return isHaving
     }
     
-    func displayWinner(_ result: String) {
-        var message: String = ""
+    func endGame(message: String) {
+        displayWinner(message)
+    }
+    
+    func displayWinner(_ message: String) {
         
-        if result == "player" {
-            message = "You won the game"
-        }
-        else if result == "draw" {
-            message = "Game is drawn"
-        }
-        else {
-            message = "Phone won the game"
-        }
-        
-        let alert = UIAlertController(title: "Winner", message: "\(message)", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Game Over", message: "\(message)", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-        self.present(alert, animated: true) {
-            self.restartGame()
-        }
+        self.present(alert, animated: true, completion: nil)
     }
     
-    func restartGame() {
-        isComputerPlayer = false
-        computer = []
-        player = []
-        availableNumbers = []
+    @IBAction func restart(_ sender: Any) {
+        isPhonePlayer = false
+        phonesCells = []
+        playersCells = []
         winner = ""
         numberOfClicks = 0
+        availableCells = [1,2,3,4,5,6,7,8,9]
         
-        clearButtonTitles()
-        print(computer)
-        print(player)
-        print(availableNumbers)
-        print(winner)
-        print(numberOfClicks)
-    }
-    
-    func clearButtonTitles() {
         for button in buttonsArray {
             button.setTitle("", for: UIControl.State.normal)
             button.backgroundColor = UIColor.white
